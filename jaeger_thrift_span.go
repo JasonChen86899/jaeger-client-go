2cc69cbc91f5db56f345f9f0257719278ef42a85
jason chen <chenhao86899@gmail.com>
2020-09-08 15:09:39 +0800

    tail_based_sampling feature

--- jaeger_thrift_span.go
+++ jaeger_thrift_span.go
@@ -15,12 +15,19 @@
 package jaeger
 
 import (
+	"net"
+	"strings"
 	"time"
 
 	"github.com/opentracing/opentracing-go"
 
 	j "github.com/uber/jaeger-client-go/thrift-gen/jaeger"
 	"github.com/uber/jaeger-client-go/utils"
+)
+
+const (
+	servicesIPsTagKey     = "tag.services.ips"
+	servicesIPsBaggageKey = "baggage.services.ips"
 )
 
 // BuildJaegerThrift builds jaeger span based on internal span.
@@ -30,6 +37,9 @@ func BuildJaegerThrift(span *Span) *j.Span {
 	defer span.Unlock()
 	startTime := utils.TimeToMicrosecondsSinceEpochInt64(span.startTime)
 	duration := span.duration.Nanoseconds() / int64(time.Microsecond)
+	// add local ips to span baggage for distribution，then need put into jaeger span tags
+	baggageServiceIps(span)
+	// build jaeger span
 	jaegerSpan := &j.Span{
 		TraceIdLow:    int64(span.context.traceID.Low),
 		TraceIdHigh:   int64(span.context.traceID.High),
@@ -39,7 +49,7 @@ func BuildJaegerThrift(span *Span) *j.Span {
 		Flags:         int32(span.context.samplingState.flags()),
 		StartTime:     startTime,
 		Duration:      duration,
-		Tags:          buildTags(span.tags, span.tracer.options.maxTagValueLength),
+		Tags:          buildTags(span.tags, span.tracer.options.maxTagValueLength, span.BaggageItem(servicesIPsBaggageKey)),
 		Logs:          buildLogs(span.logs),
 		References:    buildReferences(span.references),
 	}
@@ -51,13 +61,13 @@ func BuildJaegerThrift(span *Span) *j.Span {
 func BuildJaegerProcessThrift(span *Span) *j.Process {
 	span.Lock()
 	defer span.Unlock()
-	return buildJaegerProcessThrift(span.tracer)
+	return buildJaegerProcessThrift(span.tracer, span.BaggageItem(servicesIPsBaggageKey))
 }
 
-func buildJaegerProcessThrift(tracer *Tracer) *j.Process {
+func buildJaegerProcessThrift(tracer *Tracer, ips string) *j.Process {
 	process := &j.Process{
 		ServiceName: tracer.serviceName,
-		Tags:        buildTags(tracer.tags, tracer.options.maxTagValueLength),
+		Tags:        buildTags(tracer.tags, tracer.options.maxTagValueLength, ips),
 	}
 	if tracer.process.UUID != "" {
 		process.Tags = append(process.Tags, &j.Tag{Key: TracerUUIDTagKey, VStr: &tracer.process.UUID, VType: j.TagType_STRING})
@@ -65,12 +75,19 @@ func buildJaegerProcessThrift(tracer *Tracer) *j.Process {
 	return process
 }
 
-func buildTags(tags []Tag, maxTagValueLength int) []*j.Tag {
+func buildTags(tags []Tag, maxTagValueLength int, ips string) []*j.Tag {
 	jTags := make([]*j.Tag, 0, len(tags))
 	for _, tag := range tags {
 		jTag := buildTag(&tag, maxTagValueLength)
 		jTags = append(jTags, jTag)
 	}
+
+	// add ips tag
+	ipsJTag := buildTag(&Tag{
+		key:   servicesIPsTagKey,
+		value: ips,
+	}, maxTagValueLength)
+	jTags = append(jTags, ipsJTag)
 	return jTags
 }
 
@@ -178,4 +195,29 @@ func spanRef(ctx SpanContext, refType j.SpanRefType) *j.SpanRef {
 		TraceIdHigh: int64(ctx.traceID.High),
 		SpanId:      int64(ctx.spanID),
 	}
+}
+
+func baggageServiceIps(span *Span) {
+	ips := span.BaggageItem(servicesIPsBaggageKey)
+	if localIP, err := getLocalAddress(); err == nil && localIP != "" {
+		ips = strings.Join([]string{ips, localIP}, ",")
+	}
+	span.SetBaggageItem(servicesIPsBaggageKey, ips)
+}
+
+func getLocalAddress() (string, error) {
+	adds, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, address := range adds {
+		if ipNet, ok := address.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To16() != nil {
+				return ipNet.IP.String(), nil
+			}
+		}
+	}
+
+	return "", nil
 }
